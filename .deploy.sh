23b297cd5bfc797c3a1f13293da97b18bfcedf4e
otofune <otofune@gmail.com>
2017-05-16 00:38:49 +0900

    travis: 判定を修正
    travis.ymlにあると面倒なのと、early returnにした

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# ブランチがmasterではない、もしくはPRであった場合に終了
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  echo "ブランチがmasterではない、もしくはPull Requestである為リリースは行われません。"
+  exit 0
+fi
+
 # バージョン取得
 CURRENT_VERSION=$(grep 'Stable tag:' readme.txt | cut -d' ' -f3)
 # .git削除のためコピー
@@ -20,8 +27,8 @@ if [ ! -d "tags/$CURRENT_VERSION" ]; then
   cp -r ../push7-publishee "./tags/$CURRENT_VERSION"
   svn add "./tags/$CURRENT_VERSION"
   # コミットする
-  svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
-  [ $? -eq 0 ] && echo "バージョン ($CURRENT_VERSION) がリリースされました"
+  # svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
+  [ $? -eq 0 ] && echo "バージョン $CURRENT_VERSION がリリースされました。"
 else
-  echo "このバージョン ($CURRENT_VERSION) はすでにリリースされています"
+  echo "バージョン $CURRENT_VERSION はすでにリリースされています。"
 fi
